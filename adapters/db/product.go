bcb8e77c308731fd658a9462fccde14153fd9572
gui-meireles <guilhermemeirelesnascimento@gmail.com>
2024-07-16 21:18:25 -0300

    feature: Implementando método de Save no db

--- adapters/db/product.go
+++ adapters/db/product.go
@@ -26,3 +26,59 @@ func (p *ProductDb) Get(id string) (application.ProductInterface, error) {
 	}
 	return &product, nil
 }
+
+func (p *ProductDb) Save(product application.ProductInterface) (application.ProductInterface, error) {
+	var rows int
+
+	// Busca no banco pelo id do produto, caso exista algum produto ele atribuirá para a var 'rows'
+	p.db.QueryRow("SELECT id FROM products WHERE id=?", product.GetID()).Scan(&rows)
+
+	if rows == 0 {
+		_, err := p.create(product)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		_, err := p.update(product)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return product, nil
+}
+
+// Métodos com a 1° letra minuscula são métodos privates
+func (p *ProductDb) create(product application.ProductInterface) (application.ProductInterface, error) {
+	stmt, err := p.db.Prepare(`INSERT INTO products(id,name,price,status) VALUES(?,?,?,?);`)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = stmt.Exec(
+		product.GetID(),
+		product.GetName(),
+		product.GetPrice(),
+		product.GetStatus(),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	err = stmt.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
+}
+
+// Métodos com a 1° letra minuscula são métodos privates
+func (p *ProductDb) update(product application.ProductInterface) (application.ProductInterface, error) {
+	_, err := p.db.Exec("UPDATE products SET name = ?, price=?, status=? WHERE id=?",
+		product.GetName(), product.GetPrice(), product.GetStatus(), product.GetID())
+
+	if err != nil {
+		return nil, err
+	}
+	return product, nil
+}
